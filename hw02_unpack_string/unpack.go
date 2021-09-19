24f20f13a7148c819e6261439f3be3e8656b036c
IvanProg00 <gavrilovivan2000@gmail.com>
2021-09-19 12:08:09 +0200

    Resolved problem with clean code

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -7,46 +7,58 @@ import (
 	"unicode"
 )
 
+type unpackStruct struct {
+	b            strings.Builder
+	isPastLetter bool
+	isPastSlash  bool
+	pastLetter   rune
+}
+
 var ErrInvalidString = errors.New("invalid string")
 
 func Unpack(str string) (string, error) {
-	var b strings.Builder
-	isPastLetter := false
-	isPastSlash := false
-	var pastLetter rune
+	unpackS := unpackStruct{
+		isPastLetter: false,
+		isPastSlash:  false,
+	}
 
 	for _, c := range str {
-		if unicode.IsDigit(c) && !isPastSlash {
-			if !isPastLetter {
+		if unicode.IsDigit(c) && !unpackS.isPastSlash {
+			if !unpackS.isPastLetter {
 				return "", ErrInvalidString
 			}
 
 			numRep, _ := strconv.Atoi(string(c))
-			b.WriteString(strings.Repeat(string(pastLetter), numRep))
-			isPastLetter = false
-		} else {
-			if isPastSlash && !unicode.IsDigit(c) && c != '\\' {
-				return "", ErrInvalidString
-			}
-
-			if isPastLetter && !isPastSlash {
-				b.WriteRune(pastLetter)
-			}
-
-			if isPastSlash {
-				isPastSlash = false
-			} else if c == '\\' {
-				isPastSlash = true
-			}
-
-			pastLetter = c
-			isPastLetter = true
+			unpackS.b.WriteString(strings.Repeat(string(unpackS.pastLetter), numRep))
+			unpackS.isPastLetter = false
+		} else if err := ifLetter(&unpackS, c); err != nil {
+			return "", ErrInvalidString
 		}
 	}
 
-	if isPastLetter {
-		b.WriteRune(pastLetter)
+	if unpackS.isPastLetter {
+		unpackS.b.WriteRune(unpackS.pastLetter)
 	}
 
-	return b.String(), nil
+	return unpackS.b.String(), nil
+}
+
+func ifLetter(unpackS *unpackStruct, currentRune rune) error {
+	if unpackS.isPastSlash && !unicode.IsDigit(currentRune) && currentRune != '\\' {
+		return ErrInvalidString
+	}
+
+	if unpackS.isPastLetter && !unpackS.isPastSlash {
+		unpackS.b.WriteRune(unpackS.pastLetter)
+	}
+
+	if unpackS.isPastSlash {
+		unpackS.isPastSlash = false
+	} else if currentRune == '\\' {
+		unpackS.isPastSlash = true
+	}
+
+	unpackS.pastLetter = currentRune
+	unpackS.isPastLetter = true
+	return nil
 }
