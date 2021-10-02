a4f82a37ed9929074132c66ff3bac20ccf5b96c9
IvanProg00 <gavrilovivan2000@gmail.com>
2021-10-02 23:24:53 +0200

    Changed package dir. Made code for cache

--- hw04_lru_cache/cache.go
+++ hw04_lru_cache/cache.go
@@ -9,17 +9,15 @@ type Cache interface {
 }
 
 type lruCache struct {
-	Cache // Remove me after realization.
-
 	capacity int
 	queue    List
 	items    map[Key]*ListItem
 }
 
-type cacheItem struct {
-	key   string
-	value interface{}
-}
+// type cacheItem struct {
+// 	key   string
+// 	value interface{}
+// }
 
 func NewCache(capacity int) Cache {
 	return &lruCache{
@@ -27,4 +25,35 @@ func NewCache(capacity int) Cache {
 		queue:    NewList(),
 		items:    make(map[Key]*ListItem, capacity),
 	}
+}
+
+func (c *lruCache) Set(key Key, value interface{}) bool {
+	if i, ok := c.items[key]; ok {
+		i.Value = value
+		c.queue.MoveToFront(i)
+		return true
+	}
+
+	i := c.queue.PushFront(value)
+	if c.queue.Len() > c.capacity {
+		c.queue.Remove(c.queue.Back())
+	}
+	c.items[key] = i
+
+	return false
+}
+
+func (c *lruCache) Get(key Key) (interface{}, bool) {
+	val, ok := c.items[key]
+	if !ok {
+		return nil, false
+	}
+
+	c.queue.MoveToFront(val)
+	return val.Value, true
+}
+
+func (c *lruCache) Clear() {
+	c.items = nil
+	c.queue = nil
 }
