7c4acab7b3d12578a7da02d721b3e38da2474501
IvanProg00 <gavrilovivan2000@gmail.com>
2021-11-08 22:35:40 +0100

    Implemented code for pipeline

--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -9,6 +9,31 @@ type (
 type Stage func(in In) (out Out)
 
 func ExecutePipeline(in In, done In, stages ...Stage) Out {
-	// Place your code here.
-	return nil
+	chIn := in
+
+	for _, stage := range stages {
+		chOut := make(Bi)
+		stage := stage
+
+		go func(chIn In) {
+			defer close(chOut)
+			chStage := stage(chIn)
+
+			for {
+				select {
+				case val, ok := <-chStage:
+					if !ok {
+						return
+					}
+					chOut <- val
+				case <-done:
+					return
+				}
+			}
+		}(chIn)
+
+		chIn = chOut
+	}
+
+	return chIn
 }
