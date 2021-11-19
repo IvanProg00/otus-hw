cf50d39c51cdd04187f819ddc5e6b16de4929c06
IvanProg00 <gavrilovivan2000@gmail.com>
2021-11-20 00:24:16 +0100

    Implemented validation flags

--- hw07_file_copying/main.go
+++ hw07_file_copying/main.go
@@ -1,12 +1,18 @@
 package main
 
 import (
+	"errors"
 	"flag"
+	"fmt"
+	"os"
 )
 
 var (
 	from, to      string
 	limit, offset int64
+
+	ErrParamFromRequired = errors.New("param \"from\" is required")
+	ErrParamToRequired   = errors.New("param \"to\" is required")
 )
 
 func init() {
@@ -18,5 +24,24 @@ func init() {
 
 func main() {
 	flag.Parse()
-	// Place your code here.
+	if err := validateParams(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	if err := Copy(from, to, offset, limit); err != nil {
+		panic(err)
+	}
+}
+
+func validateParams() error {
+	if from == "" {
+		return ErrParamFromRequired
+	}
+
+	if to == "" {
+		return ErrParamToRequired
+	}
+
+	return nil
 }
