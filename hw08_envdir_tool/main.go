1004d17fb62ce3c31f3b54db7d35dbfdd929767f
IvanProg00 <gavrilovivan2000@gmail.com>
2021-11-23 15:52:36 +0100

    Implement validation of arguments

--- hw08_envdir_tool/main.go
+++ hw08_envdir_tool/main.go
@@ -1,5 +1,31 @@
 package main
 
+import (
+	"fmt"
+	"os"
+)
+
 func main() {
-	// Place your code here.
+	if len(os.Args) < 2 {
+		fmt.Println("you must pass a path to directory")
+		os.Exit(1)
+	}
+
+	path := os.Args[1]
+
+	info, err := os.Stat(path)
+	if err != nil {
+		pathError, ok := err.(*os.PathError)
+		if !ok {
+			panic(err)
+		}
+
+		fmt.Println(pathError.Err)
+		os.Exit(1)
+	}
+
+	if !info.IsDir() {
+		fmt.Println("passed path is not a directory")
+		os.Exit(1)
+	}
 }
